769cc91b33c5d3943ccee1a433f240e00c40d87c
ApprenticeHou <houhansen@163.com>
2017-08-28 20:12:52 +0800

    Add files via upload

---
+++ JOption.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class JOption {
+JOption(){
+	JFrame jr=new JFrame();
+	JOptionPane jp=new JOptionPane("jop");
+	jp.showMessageDialog(jr, "≤‚ ‘“ªœ¬");
+}
+public static void main(String[] args) {
+	JOption jo=new JOption();
+}
+}
